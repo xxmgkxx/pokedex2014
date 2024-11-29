886b184e04031b6441750680494c87adbe84eea3
Aluan <aluan.cabral@outlook.com>
2024-11-28 22:39:09 -0300

    add (pokemon list): add pagination

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,3 +1,5 @@
+// # https://github.com/xxmgkxx/pokedex2014
+
 import { useEffect, useState } from "react";
 import PokemonCard from "./PokemonCard";
 
@@ -8,7 +10,19 @@ function PokemonList() {
   }
 // https://github.com/xxmgkxx/pokedex2014
 
-  const [pokemonList, setPokemonList] = useState([])
+  const [pokemonList, setPokemonList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 20;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+  const currentPokemonList = pokemonList.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+
+  const totalPages = Math.ceil(pokemonList.length / itemsPerPage);
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
@@ -31,16 +45,23 @@ function PokemonList() {
            Voltar </button>
         </div>
       ) : (
+        <>
         <div className="pokemon-list">
-            {pokemonList.map((pokemon, index) => (
+            {currentPokemonList.map((pokemon, index) => (
                 <PokemonCard
                   key={index}
                   name={pokemon.name}
-                  image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+                  image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${((currentPage-1)*itemsPerPage) + index + 1 }.png`}
                   type="Desconhecido"
                   />
             ))}
         </div>
+        <div className="pagination">
+            <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === totalPages}>Anterior</button>
+            <span>Página {currentPage}</span>
+            <button onClick={() => setCurrentPage(currentPage + 1)}>Próxima</button>
+        </div>
+        </>
       ) }
 
 </div>
